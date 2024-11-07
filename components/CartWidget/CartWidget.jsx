cb0ba9bb389267ee2fa7121e4d2b4ae0a4cdb2bc
Guillermo Sarmiento <niko_sarmiento95@hotmail.com>
2024-11-07 18:36:02 -0300

    primera entrega ReactFlex / Sarmiento

---
+++ components/CartWidget/CartWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CartWidget = () => {
+    return  (
+        <div>
+            <img src="./src/assets/cart.png" alt="imagen de carrito" /> <span>6</span>
+        </div>
+    )
+}
+
+export default CartWidget
